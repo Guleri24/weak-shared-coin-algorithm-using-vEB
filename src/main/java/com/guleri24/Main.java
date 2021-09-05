6a26fa2f9074716a71839796b5ec7115abd2c2d1
Guleri24 <guleri24@tuta.io>
2021-09-05 22:50:52 +0530

    Bug Fixes In WeakSharedCoinAlgo
    - Solved the clustering problems while updating the overall structure

--- src/main/java/com/guleri24/Main.java
+++ src/main/java/com/guleri24/Main.java
@@ -17,11 +17,13 @@ public class Main {
     private static int weakSharedCoinAlgo(int n) {
         int K = (int) (pow(n, 2) * log(n));
         int T = (int) (4 * n * log(n));
+        int sqrt = (int) sqrt(n);
+       // int lglgn = (int) log(log(n));
 
         int count = 0;
         int var = 0;
         int total = 0;
-        vEBTree<Register> V = new vEBTree<>(n);
+        vEBTree<Register> V = new vEBTree<>(n); // overall structure created
 
 
         for (int k = 1; k <= n; k++) {
@@ -32,13 +34,15 @@ public class Main {
             total += vote;
 
             //Register R = new Register(count, var, total);
-
-            for (int j = 1; j < log(log(n)); j++) {
-                if ((k % sqrt(n)) == 0) {
+            for (int j = 1; j <= sqrt; j++) {
+                /*
+                TODO: Check : Not Sure is it required or not
+                if ((k / sqrt) == 0) {  // move to next cluster as this cluster now contains sq root of n Registers
                     break;
-                }
-                /* update the summary array where V is the overall structure */
-                V.put(j, new Register(count, var, total));
+                }*/
+
+                /* update the structure where V is the overall structure */
+                System.out.println(j + " " + V.put(j, new Register(count, var, total)));
             }
             if (k % n == 0) {
                 System.out.println(V.values());
